9361a64eb42748faa7c84863692dc0ad205af499
changbin.won <changbin.won@lge.com>
2019-06-20 14:54:52 +0900

    ETRI 소스이므로 위치 변경

--- 3_Code/Moving_camera_anomaly_detection/training_shell.sh
+++
@@ -1,3 +0,0 @@
-sleep 1h
-
-python train.py --cuda --wtl2 0.999 --niter 200
--- 3_Code/Static_Anomaly_code/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#now=$(date +%Y%m%d-%H:%M)
-#out="./output/"
-#out=$out$now
-#python train.py --dataset "lobby/1Hz" --niter 200 --iheight 12 --iwidth 24 --ngpu 1 --alpha 500 --outf $out --zdim 150 --gpu_ids 3
-
-# TODO: train.sh 와 test.sh 를 합쳐서 run.sh로 만들기?
-export PYTHONPATH=.
-python ./script/test.py --phase 'test' --dataset "resize/" --batchsize 512 --epochs 50 --iheight 64 --iwidth 64 --ngpu 1 --alpha 50 --load_weights --outf "path-to-trained-output" --zdim 150 --extralayers 0
